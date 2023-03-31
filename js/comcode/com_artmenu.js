8ffed56a9c2d44b07e9ea0515d99d299ad51a44f
ArbeiterKlassen <deepdarkfantasyvan@outlook.com>
2023-03-31 13:10:33 +0800

    reopen

---
+++ js/comcode/com_artmenu.js
@@ -0,0 +1,135 @@
+var artmenu = [
+    {type:"art",subtype:"essay",links:"../essays/论哲学归宿，保卫哲学和继续实践.pdf",time:"2023-1-24",author:"ArbeiterKlassen",name:"论哲学归宿，保卫哲学和继续实践",headpic:"https://i.postimg.cc/FRQcPq8w/marx1.jpg",detail:"历史政治经济运动和其反映出的社会运动不仅仅要反过来反映其时代思想，还要反过来指向政治经济和社会文化各个层面的社会矛盾。那么，研究历史逻辑的过程必须形成对社会矛盾的认识，而得到这些认识——就自发地将它们应用到现代社会政治经济的矛盾分析中。这个过程——认识和分析世界的过程是哲学的发端及其归宿。"},
+    {type:"art",subtype:"normal",links:"../articles/关于深圳中学学生自学驱动力及学习方法的来源的见解.html",time:"2023-1-22",author:"蚌埠黄豆不怕流汗",name:"关于深圳中学学生自学驱动力及学习方法的来源的见解",headpic:"https://i.postimg.cc/ZKYPtGnS/hengshui1.jpg",detail:"被统治阶级往往具有一种愚昧性，但这并不是被统治阶级的错，而是统治阶级在精神上奴役被统治阶级的表现。"},
+    {type:"art",subtype:"normal",links:"../articles/对文化自信的一点讨论.html",time:"2023-1-12",author:"SW-Eril",name:"对文化自信的一点讨论",headpic:"https://i.postimg.cc/XYzyG40v/captitalism1.jpg",detail:"资产阶级现在所宣传的反动文化所表现的核心行为会造成文化的侵略性和“正确性”。"},
+    {type:"art",subtype:"normal",links:"../articles/二〇二三-我们这样团结.html",time:"2023-1-11",author:"蚌埠黄豆不怕流汗",name:"二〇二三-我们这样团结",headpic:"https://i.postimg.cc/XYzyG40v/captitalism1.jpg",detail:"同志们，今天我们在水深火热中庆祝将至的兔年春节。"},
+    {type:"art",subtype:"normal",links:"../articles/论苏联解体和修正主义.html",time:"2022-12-25",author:"浮生",name:"论苏联解体和修正主义",headpic:"https://i.postimg.cc/J4wndQT3/revisionism1.png",detail:""},
+    {type:"art",subtype:"essay",links:"../articles/自由论-警惕自由主义的腐蚀.html",time:"2022-12-25",author:"浮生",name:"自由论-警惕自由主义的腐蚀",headpic:"https://i.postimg.cc/2yW5Sq5B/critic-of-freedom1.png",detail:""},
+    {type:"art",subtype:"normal",links:"../articles/中国领导下的人口问题.html",time:"2022-12-25",author:"浮生",name:"中国领导下的人口问题",headpic:"https://i.postimg.cc/J4wndQT3/revisionism1.png",detail:""},
+    {type:"art",subtype:"normal",links:"../articles/哲学世界的两大家族.html",time:"2022-12-25",author:"浮生",name:"哲学世界的两大家族",headpic:"https://i.postimg.cc/50T0J5fV/philosophy1.jpg",detail:""},
+    {type:"art",subtype:"normal",links:"../articles/一线汽修工人采访记录.html",time:"2022-12-25",author:"铃秋",name:"一线汽修工人采访记录",headpic:"https://i.postimg.cc/qvDXKYLX/worker1.jpg",detail:""},
+    {type:"art",subtype:"normal",links:"../articles/家庭和爱情.html",time:"2022-12-25",author:"八小时 稻公",name:"家庭和爱情",headpic:"https://i.postimg.cc/FRQcPq8w/marx1.jpg",detail:""},
+    {type:"art",subtype:"essay",links:"../articles/集权论-法西斯主义是什么.html",time:"2022-12-25",author:"浮生",name:"集权论-法西斯主义是什么",headpic:"https://i.postimg.cc/vBBgbH9W/anti-Fascism1.jpg",detail:""},
+    {type:"art",subtype:"normal",links:"../articles/何去何从-社会主义还是沙文主义.html",time:"2022-12-25",author:"浮生",name:"何去何从-社会主义还是沙文主义",headpic:"https://i.postimg.cc/PfvJ47Mr/jingoism1.png",detail:""},
+    {type:"art",subtype:"normal",links:"../articles/孤独摇滚.html",time:"2022-12-25",author:"超级阿伏伽德罗",name:"孤独摇滚",headpic:"https://i.postimg.cc/FRQcPq8w/marx1.jpg",detail:""},
+    {type:"art",subtype:"essay",links:"../articles/斗争论-无产阶级的阶级斗争.html",time:"2022-12-25",author:"浮生",name:"斗争论-无产阶级的阶级斗争",headpic:"https://i.postimg.cc/15ptxNTk/kampf1.png",detail:""},
+    {type:"art",subtype:"normal",links:"../articles/站稳政治立场，坚定政治信仰.html",time:"2022-11-29",author:"笑笑",name:"站稳政治立场，坚定政治信仰",headpic:"https://i.postimg.cc/fLjd1Chy/mao4.jpg",detail:""},
+    {type:"art",subtype:"normal",links:"../articles/和我们队伍中的民粹主义者划清界限.html",time:"2022-11-04",author:"苏维埃小九",name:"和我们队伍中的民粹主义者划清界限",headpic:"https://i.postimg.cc/TYHb8hBV/populism1.jpg",detail:""},
+    {type:"art",subtype:"normal",links:"../articles/对于革命的路线问题的一个论述.html",time:"2022-10-08",author:"苏维埃小九",name:"对于革命的路线问题的一个论述",headpic:"https://i.postimg.cc/x1wbChfq/AnaCom1.jpg",detail:""},
+    {type:"art",subtype:"normal",links:"../articles/【小说】九篇小说.html",time:"2022-01-31",author:"奇才",name:"【小说】九篇小说",headpic:"https://i.postimg.cc/RZywtqRS/anti-Revision1.jpg",detail:""},
+    {type:"art",subtype:"normal",links:"../articles/【转载】无政府主义共产主义者.html",time:"2022-01-22",author:"阿兰-彭加姆",name:"【转载】无政府主义共产主义者",headpic:"https://i.postimg.cc/x1wbChfq/AnaCom1.jpg",detail:""},
+    {type:"art",subtype:"normal",links:"../articles/中共（修）研究.html",time:"2022-01-22",author:"奇才",name:"中共（修）研究",headpic:"https://i.postimg.cc/RZywtqRS/anti-Revision1.jpg",detail:""},
+    {type:"art",subtype:"normal",links:"../articles/【转载】请看苏修的新玩意儿.html",time:"2022-01-21",author:"《红旗》",name:"【转载】请看苏修的新玩意儿",headpic:"https://i.postimg.cc/43cVQZLz/mao3.jpg",detail:""},
+    {type:"art",subtype:"normal",links:"../articles/太阳照常升起.html",time:"2021-09-09",author:"ArbeiterKlassen",name:"太阳照常升起",headpic:"https://i.postimg.cc/43cVQZLz/mao3.jpg",detail:""},
+    {type:"art",subtype:"normal",links:"../articles/三篇读后杂记.html",time:"2021-08-27",author:"ArbeiterKlassen",name:"三篇读后杂记",headpic:"https://i.postimg.cc/FRQcPq8w/marx1.jpg",detail:"人类社会的历史自古分为几个阶段：远古时期——原始社会——奴隶社会——封建社会——资本主义社会——现今。"},
+    {type:"art",subtype:"normal",links:"../articles/浅谈教育内卷化.html",time:"2021-08-22",author:"ArbeiterKlassen",name:"浅谈教育内卷化",headpic:"https://i.postimg.cc/FsKSn0CF/Involution1.jpg",detail:"“内卷化”成了一个流行词。在学生群体中，又引申出了“教育内卷化”这一在学生中的具体体现。要谈教育内卷化，就首先要讲清楚什么是内卷化。"},
+    {type:"art",subtype:"normal",links:"../articles/简单论述近两年中国国内爆发式出现的民族主义.html",time:"2021-08-21",author:"有为青年",name:"简单论述近两年中国国内爆发式出现的民族主义",headpic:"https://i.postimg.cc/VsW9hfYg/anti-Racism1.jpg",detail:""},
+    {type:"art",subtype:"essay",links:"../articles/论中国国家资本.html",time:"2021-08-21",author:"ArbeiterKlassen",name:"论中国国家资本",headpic:"https://i.postimg.cc/FRQcPq8w/marx1.jpg",detail:"个人垄断的资本主义向国家参与经济管理发展后的产物，应当是相比较而言更加进步的资本主义。在实际作用上，应当对比的看待几大时期的资本主义制度。"},
+    {type:"art",subtype:"normal",links:"../articles/评当前补习班教育问题.html",time:"2021-08-19",author:"ArbeiterKlassen",name:"评当前补习班教育问题",headpic:"https://i.postimg.cc/ZKYPtGnS/hengshui1.jpg",detail:"社会上的基础设施——教育，医疗，工业开始过度市场化。失去了国家政府的管控，自由的市场经济——自由的资本主义开始垄断各行各业。抛去网络，医疗，工业，剩下一个教育成为了金融资本的角逐地。"},
+    {type:"art",subtype:"normal",links:"../articles/谈近期的反黑人思潮.html",time:"2021-08-17",author:"ArbeiterKlassen",name:"谈近期的反黑人思潮",headpic:"https://i.postimg.cc/VsW9hfYg/anti-Racism1.jpg",detail:"随着我国改革开放的推进，中国的综合国力不断增强；同时，经济实力的不断提高，吸引了一大批外国籍人士来华。2000年之后，随着我国与大量亚非国家共创的“一带一路”建设的不断推进，大批非洲国家人士来华留学经商。大批非洲国家人士——或者直说黑人入境，直接引发了两个问题——怎么安置外国人？怎么指定对外国人的相应政策？"},
+    {type:"art",subtype:"essay",links:"../articles/怎么办（1）.html",time:"2021-07-23",author:"ArbeiterKlassen",name:"怎么办（1）",headpic:"https://i.postimg.cc/RVP7MgJn/mao1.jpg",detail:"进入20世纪，俄国首先爆发了1917年的十月革命，一声炮响给中国人民带来了马克思主义。紧接着是1915年中国新文化运动向马克思主义的转变和1919年五四运动的爆发，青年学生和工人站在了革命的前线。1921年，中国共产党成立并开始领导中国工人运动。"},
+    {type:"art",subtype:"normal",links:"../articles/谈巴以冲突.html",time:"2021-06-29",author:"ArbeiterKlassen",name:"谈巴以冲突",headpic:"https://i.postimg.cc/RVP7MgJn/mao1.jpg",detail:":民族矛盾的根源还是阶级矛盾。马克思主义认为，民族矛盾的表象是宗教、民族、领土冲突，实质是不同阶级的矛盾，即民族矛盾的本质还是阶级矛盾。"},
+    {type:"art",subtype:"essay",links:"../articles/论信息化资本主义.html",time:"2021-05-06",author:"Faschiung",name:"论信息化资本主义",headpic:"https://i.postimg.cc/FRQcPq8w/marx1.jpg",detail:"自20世纪40至50年代起，电子计算——即后来的计算机的兴起促使社会生产力大幅提高。90年代后互联网在全球普及，美国的苹果、微软等公司腾飞；"},
+    {type:"art",subtype:"essay",links:"../articles/中产阶级扩大论.html",time:"2021-03-13",author:"Faschiung",name:"中产阶级扩大论",headpic:"https://i.postimg.cc/FRQcPq8w/marx1.jpg",detail:"中产阶级，即小资产阶级实际上是无产阶级中的特殊组成部分，占有少部分的生产资料，但不属于大资产阶级。既可以单独作为社会阶级之一，也可以划分进无产阶级之内。"},
+
+    {type:"doc",subtype:"normal",links:"../docs/doc-2022-09-12.pdf",time:"2022-09-12",author:"赤原社",name:"二〇二二年九月八日和九月十五日社内事务文件",headpic:"",detail:""},
+    {type:"doc",subtype:"normal",links:"../docs/doc-2022-09-14.pdf",time:"2022-09-14",author:"赤原社",name:"二〇二二年社团纳新登记表",headpic:"",detail:""},
+    {type:"doc",subtype:"normal",links:"../docs/doc-2022-09-15.pdf",time:"2022-09-15",author:"赤原社",name:"二〇二二年社团纳新记录表",headpic:"",detail:""},
+    {type:"doc",subtype:"normal",links:"../docs/doc-2022-09-16.pdf",time:"2022-09-16",author:"赤原社",name:"二〇二二年十月六日社团财务运营报表",headpic:"",detail:""},
+    {type:"doc",subtype:"normal",links:"../docs/doc-2022-11-19.pdf",time:"2022-11-19",author:"内蒙古左翼社团联合会",name:"关于成立内蒙古左翼社团联合会的决议的草案研究会议记录",headpic:"",detail:""},
+    {type:"doc",subtype:"normal",links:"../docs/doc-2022-12-07.pdf",time:"2022-12-07",author:"赤原社",name:"包头九中赤原青年马克思主义学社（赤原社）章程 第二版",headpic:"",detail:""},
+
+    {type:"arc",subtype:"normal",links:"./archive.html",time:"2022-09-30",author:"赤原社",name:"赤原社 文创产品 RPC-Creations:Collections 2022 手账本 A5size 封面图[PNG]",headpic:"",detail:""},
+    {type:"arc",subtype:"normal",links:"./archive.html",time:"2022-09-30",author:"赤原社",name:"赤原社 文创产品 RPC-Creations:Collections 2022 文创全购纪念卡 一批次 N-002",headpic:"",detail:""},
+    {type:"arc",subtype:"normal",links:"./archive.html",time:"2022-09-30",author:"赤原社",name:"赤原社 文创宣传 RPC-Creations: 2022 招新特供 明信片 5.5cm*8.8cm 双色版",headpic:"",detail:""},
+    {type:"arc",subtype:"normal",links:"./archive.html",time:"2022-09-30",author:"赤原社",name:"赤原社 文创宣传 2022校内招新 宣传图&海报&社徽[PNG/JPG]",headpic:"",detail:""},
+    {type:"arc",subtype:"normal",links:"./archive.html",time:"2022-09-30",author:"赤原社",name:"赤原社 招新 2022 记录照片"},
+    {type:"arc",subtype:"normal",links:"./archive.html",time:"2022-09-30",author:"马克思，恩格斯，列宁，斯大林，毛泽东",name:"文件备案 历史资料 马恩列斯毛著作文选/全集和各时期著作[PDF]",headpic:"",detail:""},
+    {type:"arc",subtype:"normal",links:"./archive.html",time:"2022-09-30",author:"《红旗》",name:"文件备案 历史资料 《红旗》1958-1976 汇总[PDF]",headpic:"",detail:""},
+    {type:"arc",subtype:"normal",links:"./archive.html",time:"2022-09-30",author:"未知",name:"文件备案 历史资料 千禧年后社会调研分析报告[PDF]",headpic:"",detail:""},
+//{links,time,author,name},
+]
+var eachpage = 10;
+var max = artmenu.length;
+var count = 0;
+for(var i = 0; i<max;i++){if(artmenu[i].type=='art'){count+=1}};
+var pages = (count - count%eachpage)/eachpage + 1;
+var initpage = 1;
+var x;
+function print_page(pagename){
+    if(pagename == pages){x = count%eachpage+(pagename-1)*eachpage}
+    else{x = 10 + (pagename-1)*eachpage};
+    for(var i = (pagename-1)*eachpage;i<x;){
+        if(artmenu[i].type=="art"){
+            document.getElementById('pages-container').innerHTML += '<div class="article-box">'+'<div class="ab-content">'+'<a href="'+artmenu[i].links+'" class="article-img-box">'+'<img class="lazy-image article-img" data-src="'+artmenu[i].headpic+'" alt="" src="'+artmenu[i].headpic+'">'+'</a>'+'<div class="article-title">'+'<a href="'+artmenu[i].links+'">'+artmenu[i].name+'</a>'+'</div>'+'<div class="article-detail-box c-666">'+artmenu[i].detail+'</div>'+'<span class="article-tail-box">'+'<span class="article-date c-999">'+artmenu[i].time+'</span>'+'<span class="article-author one-line-overflow c-999">'+artmenu[i].author+'</span>'+'</span>'+'</div>'+'</div>'
+            i++;
+        }
+    }
+};
+var searchmode = 1;
+function clearcontent(elementID) {
+    document.getElementById(elementID).innerHTML = "";
+}
+function print_content(){
+    document.write('<li class="column-title">')
+    document.write('<span class="at-sort b-b-ece fl"><a class="at-sort-comment-a c-666 fl">文章</a></span>')
+    document.write('</li>')
+    var max = artmenu.length;
+    var inity=2020;
+    var date;
+    for(var i = max-1; i>=0;i--){if(artmenu[i].type=='art'){
+        date = artmenu[i].time.split("-");
+        if(parseInt(date[0]) > inity){inity++;document.write('<li class="person-intro-detail gradient-text">'+inity+'</li>');}
+        document.write('<li class="person-intro-detail"><span class="date">'+date[1]+'-'+date[2]+' </span><a href="'+artmenu[i].links+'" class="title">'+artmenu[i].name+'</a></li>');
+    }};
+}
+function output(){
+    clearcontent('1');
+    clearcontent('2');
+    clearcontent('3');
+    var str = document.getElementById('inpele').value;
+    str = str.trim().split(" ");//字符串拆分
+    var ifcan = new Array (artmenu.length);
+    for(var i = 0;i<artmenu.length;i++)ifcan[i] = true;
+    document.getElementById('3').innerHTML += "<br><br>";
+    document.getElementById('3').innerHTML += '<li class="person-intro-detail"><h2 style="text-align:center">搜索结果</h2><hr></li><br>';
+    var time = 0;
+    if(searchmode == 1)for(var i = 0;i<artmenu.length;i++){
+        for(var j = 0;j<str.length;j++){
+            if((artmenu[i].time.indexOf(str[j]) != -1 || artmenu[i].name.indexOf(str[j]) != -1 || artmenu[i].author.indexOf(str[j]) != -1) && ifcan[i])
+            {
+                ifcan[i] = true;
+            }else{
+                ifcan[i] = false;
+            }
+        }
+    };
+    if(searchmode == 2){
+        for(var i = 0;i<artmenu.length;i++)ifcan[i] = false;
+        for(var i = 0;i<artmenu.length;i++){
+            for(var j = 0;j<str.length;j++){
+                if((artmenu[i].time.indexOf(str[j]) != -1 || artmenu[i].name.indexOf(str[j]) != -1 || artmenu[i].author.indexOf(str[j]) != -1))
+                {
+                    ifcan[i] = true;
+                }
+            }
+        }
+    };
+    for(var i = 0;i<artmenu.length;i++){
+        if(ifcan[i]){
+            time += 1;
+            document.getElementById('3').innerHTML += '<ul class="link-box"><li class="person-intro-detail"><strong>'+artmenu[i].name+'</strong><a href="'+artmenu[i].links+'">[点此前往]</a><p style="text-align: right">作者：'+artmenu[i].author+" "+artmenu[i].time+' '+'</p><hr></li></ul>';
+        }
+    }
+    if(time == 0){
+        document.getElementById('3').innerHTML += '<<ul class="link-box"><li class="person-intro-detail"><h3>No Result</h3><hr></li></ul><br>';
+    }
+}
+function print_essays(){
+    var max = artmenu.length;
+    for(var i = 0;i<max;i++){
+        if(artmenu[i].subtype == "essay"){
+            document.write('<a href="'+artmenu[i].links+'">'+artmenu[i].author+','+artmenu[i].name+'</a><br>');
+        }
+    }
+}
