481d98bbf3359190942f828c8f344d62d680efb0
ArbeiterKlassen <deepdarkfantasyvan@outlook.com>
2023-05-27 23:03:20 +0800

    .

---
+++ js/comcode/com_arthead.js
@@ -0,0 +1,31 @@
+function getParams(key) {
+    var reg = new RegExp("(^|&)" + key + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return unescape(r[2]);
+    }
+    return null;
+};
+var mode = 1;
+if(getParams("mode") != null){mode = getParams("mode")};
+if(mode == 1){
+
+    document.write('<link rel="stylesheet" href="../css/global.css">');
+    document.write('<link rel="stylesheet" href="../css/pace-theme-flash.css">');
+    document.write('<link rel="stylesheet" href="../css/d-audio.css">');
+    document.write('<link rel="stylesheet" href="../css/article-detail.css">')
+    document.write('<link rel="stylesheet" href="../css/code.css">');
+    document.write('<link rel="stylesheet" href="../css/github-markdown.css">');
+    document.write('<link rel="stylesheet" href="../css/vditor.css">')
+    document.write('<link rel="shortcut icon" href="../img/user.jpeg">');
+}
+if(mode == 2){
+    document.write('<link rel="stylesheet" href="../css/night.global.css">');
+    document.write('<link rel="stylesheet" href="../css/pace-theme-flash.css">');
+    document.write('<link rel="stylesheet" href="../css/d-audio.css">');
+    document.write('<link rel="stylesheet" href="../css/article-detail.css">')
+    document.write('<link rel="stylesheet" href="../css/code.css">');
+    document.write('<link rel="stylesheet" href="../css/github-markdown.css">');
+    document.write('<link rel="stylesheet" href="../css/vditor.css">')
+    document.write('<link rel="shortcut icon" href="../img/user.jpeg">');
+}            
