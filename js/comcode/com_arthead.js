91216b6dad05905d1bcaabe7997ec8b4222ca54d
ArbeiterKlassen <deepdarkfantasyvan@outlook.com>
2023-05-27 23:21:09 +0800

    .

--- js/comcode/com_arthead.js
+++ js/comcode/com_arthead.js
@@ -23,9 +23,9 @@ if(mode == 2){
     document.write('<link rel="stylesheet" href="../css/night.global.css">');
     document.write('<link rel="stylesheet" href="../css/pace-theme-flash.css">');
     document.write('<link rel="stylesheet" href="../css/d-audio.css">');
-    document.write('<link rel="stylesheet" href="../css/article-detail.css">')
+    document.write('<link rel="stylesheet" href="../css/night.article-detail.css">')
     document.write('<link rel="stylesheet" href="../css/code.css">');
-    document.write('<link rel="stylesheet" href="../css/github-markdown.css">');
+    document.write('<link rel="stylesheet" href="../css/night.github-markdown.css">');
     document.write('<link rel="stylesheet" href="../css/vditor.css">')
     document.write('<link rel="shortcut icon" href="../img/user.jpeg">');
 }            
